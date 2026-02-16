10134127908d96e361e5d1d347f0dcbe2e3768cf
Martim <244059483+mq3z7i@users.noreply.github.com>
2026-02-16 17:07:12 +0000

    Create PrepareDisplayPrice.lua

---
+++ PrepareDisplayPrice.lua
@@ -0,0 +1,20 @@
+-- DisplayPrice.lua
+-- Formats a price with optional Robux icon
+
+local ICONS = {
+	Robux = ""
+}
+
+local function prepare(price: number, isRobux: boolean): string
+	if typeof(price) ~= "number" or typeof(isRobux) ~= "boolean" then
+		return "Buy"
+	end
+
+	if isRobux then
+		return ICONS.Robux .. tostring(price)
+	else
+		return tostring(price)
+	end
+end
+
+return prepare
